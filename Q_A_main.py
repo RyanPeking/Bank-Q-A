d5f55ad5b4636d3b69913a856d794806927fcbba
Ryan Wu <wsy2008401>
2019-11-21 11:11:28 +0800

    chat mode

--- Q_A_main.py
+++ Q_A_main.py
@@ -1,13 +1,42 @@
 from model.get_similar_question_by_boolean import get_similiar_qa
 import os
+import pickle
+from model.data import Vocab
 
-query = '转账'
+
 k = 10
-
 root_path = os.path.abspath('./')
 qa_path = os.path.join(root_path, 'dataset', 'clean_qa_corpus.csv')
-vocab_path = os.path.join(root_path, 'dataset', 'words_frequences.txt')
+words_frequences_path = os.path.join(root_path, 'dataset', 'words_frequences.txt')
+# vocab_path = os.path.join(root_path, 'dataset', 'vocab.pk')
 q_codes_path = os.path.join(root_path, 'dataset', 'q_codes.pk')
 
-res = get_similiar_qa(query, vocab_path, qa_path, q_codes_path, k)
-print(res)
+
+def load_pickle(path):
+    f = open(path, 'rb')
+    data = pickle.load(f)
+    return data
+
+# 加载q_codes和vocab
+q_codes = load_pickle(q_codes_path)
+# vocab = load_pickle(vocab_path)
+vocab = Vocab(words_frequences_path)
+
+
+
+print("您好，我是智能客服小龙人，请问有什么能帮助您的。您可输入您需要办理的业务，例如：银行卡开户。")
+while True:
+    query = input()
+    res = get_similiar_qa(query, vocab, qa_path, q_codes, k)
+
+    if res == None:
+        print("对不起，小龙人暂时没有该功能呢")
+
+    # 编辑距离为0，返回答案，否则返回相似问题，让用户确认答案
+    if res[0][0] == 0:
+        print(res[0][2])
+    else:
+        print('请选择您需要办理的业务：')
+        for qa in res:
+            print(qa[1])
+
--- model/get_similar_question_by_boolean.py
+++ model/get_similar_question_by_boolean.py
@@ -1,43 +1,34 @@
-import pickle
-import time
-from model.data import Vocab
 from model.utils import cut, token, topk
 import pandas as pd
 from model.get_similar_question_by_edit_distance import Edit_Distance
 
 
-def load_pickle(path):
-    f = open(path, 'rb')
-    data = pickle.load(f)
-    return data
-
-
-def get_code(query, vocab_path):
+def get_code(query, vocab):
     query = cut(query)
     # vocab = load_pickle(vocab_path)
-    vocab = Vocab(vocab_path)
     code = 0
     for word in query:
         try:
             _id = vocab.word_to_id(word)
+        # oov
         except:
             continue
         code |= 1 << _id
     return code
 
 
-def get_similiar_qa(query, vocab_path, qa_path, q_codes_path, k):
+def get_similiar_qa(query, vocab, qa_path, q_codes, k):
     qa = pd.read_csv(qa_path)
     query = token(query)
-    query_code = get_code(query, vocab_path)
-    q_codes = load_pickle(q_codes_path)
+    query_code = get_code(query, vocab)
+
     res = []
     for i, (q, a) in enumerate(zip(qa['question'], qa['answer'])):
         code_and = query_code & q_codes[i]
         if code_and == 0:
             continue
 
-        # 得到similiar中1位的个数
+        # 得到code_and中1位的个数
         # same_words_num = 0
         # while code_and:
         #     if code_and & 1:
@@ -54,20 +45,20 @@ def get_similiar_qa(query, vocab_path, qa_path, q_codes_path, k):
     for q, a in res:
         edit_distance = Edit_Distance(query, q)
         distances.append((edit_distance, q, a))
-    return topk(distances, k)
+    return sorted(topk(distances, k), key=lambda x: x[0])
 
 
-if __name__ == '__main__':
-    strat = time.time()
-    query = '转账'
-    k = 10
-    import os
-    root_path = os.path.abspath('../')
-    qa_path = os.path.join(root_path, 'dataset', 'clean_qa_corpus.csv')
-    # vocab_path = os.path.join(root_path, 'dataset', 'vocab.pk')
-    vocab_path = os.path.join(root_path, 'dataset', 'words_frequences.txt')
-    q_codes_path = os.path.join(root_path, 'dataset', 'q_codes.pk')
-    res = get_similiar_qa(query, vocab_path, qa_path, q_codes_path, k)
-    print(len(res), res)
-    end = time.time()
-    print(end-strat)
+# if __name__ == '__main__':
+#     strat = time.time()
+#     query = '银行卡开户'
+#     k = 10
+#     import os
+#     root_path = os.path.abspath('../')
+#     qa_path = os.path.join(root_path, 'dataset', 'clean_qa_corpus.csv')
+#     # vocab_path = os.path.join(root_path, 'dataset', 'vocab.pk')
+#     vocab_path = os.path.join(root_path, 'dataset', 'words_frequences.txt')
+#     q_codes_path = os.path.join(root_path, 'dataset', 'q_codes.pk')
+#     res = get_similiar_qa(query, vocab_path, qa_path, q_codes_path, k)
+#     print(len(res), res)
+#     end = time.time()
+#     print(end-strat)
